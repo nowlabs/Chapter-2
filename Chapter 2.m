a2bd59d59071fb205cb540b672812fb49f051089
Thomas Eapen <thomas@nowlabs.net>
2009-12-22 01:58:41 -0500

    The BOOL party example

--- Chapter 2.m
+++ Chapter 2.m
@@ -1,6 +1,27 @@
 #import <Foundation/Foundation.h>
 
+BOOL areIntsDifferent(int number1, int number2) {
+	if (number1 == number2) {
+		return NO;
+	} else {
+		return YES;
+	}
+}
+
+NSString *boolString(BOOL yesNo) {
+	if (yesNo == NO) {
+		return @"NO";
+	} else {
+		return @"YES";
+	}
+
+}
+
 int main (int argc, const char * argv[]) {
-    NSLog(@"Hello, World!");
+	BOOL areTheyDifferent;
+	areTheyDifferent = areIntsDifferent(5,5);
+	NSLog(@"Are %d and %d different? %@", 5, 5, boolString(areTheyDifferent));
+	areTheyDifferent = areIntsDifferent(23, 42);
+	NSLog(@"Are %d and %d different? %@", 23, 42, boolString(areTheyDifferent));
     return 0;
 }
